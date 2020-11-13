9ac2d493dec58fffca3825c16fe6a3e6fd5df989
Thomas KLING <klingthomas67@gmail.com>
2020-11-13 17:03:05 +0100

    Forgot gitignore

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.listen(4201, () => {
-    return console.log('My Node App listening on port 4201');
-});
